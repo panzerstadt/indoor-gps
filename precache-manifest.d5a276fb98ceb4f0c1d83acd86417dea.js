7c7209bc68ca156a2b1e1d495298395be4acb152
panzerstadt <tliqun@gmail.com>
2019-04-09 01:10:31 +0900

    Updates

---
+++ precache-manifest.d5a276fb98ceb4f0c1d83acd86417dea.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "11be6a407f71610d05cf35e895aab424",
+    "url": "/indoor-gps/static/media/grapes.11be6a40.svg"
+  },
+  {
+    "revision": "ed7db6eef0e1d5d99d34",
+    "url": "/indoor-gps/static/css/main.a509ecb1.chunk.css"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/indoor-gps/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "404d48e72f5201a98236",
+    "url": "/indoor-gps/static/js/1.404d48e7.chunk.js"
+  },
+  {
+    "revision": "fc62f611e4383eddc067",
+    "url": "/indoor-gps/static/js/runtime~main.fc62f611.js"
+  },
+  {
+    "revision": "eb12cece10d98a7b9496a484b47b5014",
+    "url": "/indoor-gps/static/media/map-01-med-cropped.eb12cece.png"
+  },
+  {
+    "revision": "05c81209111d504add8e4333f35f4d4d",
+    "url": "/indoor-gps/static/media/class_coords.05c81209.csv"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/indoor-gps/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f41c8e05f62e7e4dfd885ee44b2a8f5",
+    "url": "/indoor-gps/static/media/arrow.7f41c8e0.svg"
+  },
+  {
+    "revision": "ed7db6eef0e1d5d99d34",
+    "url": "/indoor-gps/static/js/main.ed7db6ee.chunk.js"
+  },
+  {
+    "revision": "42d5da8a59c46e4b0451daadc51632a6",
+    "url": "/indoor-gps/static/media/location-blue.42d5da8a.svg"
+  },
+  {
+    "revision": "fc82852fdd260b7e98d69356d99cbea7",
+    "url": "/indoor-gps/static/media/location-green.fc82852f.svg"
+  },
+  {
+    "revision": "6c8c363d22df7b7ac4d0dbb5fd620072",
+    "url": "/indoor-gps/static/media/location-red.6c8c363d.svg"
+  },
+  {
+    "revision": "ba199f9c284e33d57667878570962b66",
+    "url": "/indoor-gps/static/media/tomato.ba199f9c.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/indoor-gps/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/indoor-gps/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/indoor-gps/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "404d48e72f5201a98236",
+    "url": "/indoor-gps/static/css/1.7934f26f.chunk.css"
+  },
+  {
+    "revision": "7af072e8f16b1d58cab46e09e89bfb1b",
+    "url": "/indoor-gps/index.html"
+  }
+];
