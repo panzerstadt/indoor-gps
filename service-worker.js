7a17c1a2c32ef436f5f21665a3ca2d12968fbc87
panzerstadt <tliqun@gmail.com>
2019-08-18 08:23:53 +0000

    [skip ci] Deployment

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://panzerstadt.github.io/indoor-gps/precache-manifest.7e780efbd69dc42f1191791c5ada755d.js"
+  "https://panzerstadt.github.io/indoor-gps/precache-manifest.c9f57bc100128262fa4b766ea39400e3.js"
 );
 
 workbox.clientsClaim();
